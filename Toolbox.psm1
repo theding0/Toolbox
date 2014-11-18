3af4a606260832a31b9596dc2992d09ac47a3b63
theding0 <a.hulse90@gmail.com>
2014-11-18 11:42:36 -0600

    Add Get-Telnet
    Added Get-Telnet as a telnet replacement from command prompt

--- Toolbox.psm1
+++ Toolbox.psm1
@@ -9,16 +9,17 @@ function Connect-O365{
     [CmdletBinding()]
     param(
 	$cred
+
     )
-    BEGIN 
+    BEGIN
     {
 
         $cred = Get-Credential
 
     }
-    PROCESS 
+    PROCESS
     {
-	    $session365 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Credential $cred -Authentication Basic -AllowRedirection 
+	    $session365 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Credential $cred -Authentication Basic -AllowRedirection
         Connect-MsolService -Credential $cred
     }
     END
@@ -55,54 +56,127 @@ function Get-Distro {
         [string[]]$User
     )
     BEGIN {
-    
+
         $Group = @(Get-DistributionGroup -ResultSize Unlimited)
         $ucount = 0
         $Distros = @()
-        
 
-        
+
+
 
     } # End BEGIN block
 
-        
+
     PROCESS {
-     
+
         foreach ($u in $User) {
-    
+
             $Distros += New-object -TypeName psobject -Property @{
                 Name = $((Get-Mailbox $u).displayname)
                 MemberOf = @()
-               
-            } # End property hash 
-            
+
+            } # End property hash
+
             $i = 0
             $user_dn = $((Get-Mailbox $u).distinguishedname)
-           
+
             foreach ($g in $Group) {
-            
-                    
+
+
                 Write-Progress -Activity "Collecting distribution groups for $((Get-Mailbox ($u)).displayname)" -Status "Checking $g" -PercentComplete ($i / $Group.Count * 100)
-               
+
                 if ((Get-DistributionGroupMember $g.identity | select -expand distinguishedname) -contains $user_dn) {
-                    
+
                     $Distros[$ucount].MemberOf += $g
-                                
+
                  } # End identity check
 
-                    $i++  
+                    $i++
+
              } # End foreach $Group
              $ucount++
-       
-        
+
     } # End foreach $User
-        
+
 } #End PROCESS block
 
     END {
-    
-    Write-Output ($Distros | sort name ) 
+
+    Write-Output $Distros 
+
 
 
     } # End END block
 } # End Get-Distro function
+
+
+<#
+.SYNOPSIS
+    Command prompt Telnet replacement
+
+.DESCRIPTION
+    Creates a socket object and attempts to connect that object to the specified server on the specified port
+
+.PARAMETER Server
+    Multiple servers can be tested by comma seperated IP addresses 
+
+.PARAMETER Port
+    Port number to be tested
+
+.EXAMPLE
+    Get-Telnet 10.150.1.10 25
+
+    Test a single server for a connection on SMTP port
+
+.EXAMPLE
+    Get-Telnet 10.150.1.10,10.150.1.12 80
+
+    Check for open HTTP port on multiple servers
+
+.OUTPUT
+    PSObject Array
+
+#>
+function Get-Telnet {
+    param(
+    [Parameter(Position=0,Mandatory=$true)]
+    [string[]]$Server,
+    [Parameter(Position=1,Mandatory=$true)]
+    [int]$Port
+    )
+    BEGIN {
+    $Results = @()
+    }
+
+    PROCESS {
+    foreach ($s in $Server) {
+                        
+            # Test and enumerate servers with TCP port open
+            # Create a Net.Sockets.TcpClient object to use for checking for open TCP ports
+            $Socket = New-Object Net.Sockets.TcpClient
+
+            # Suppress error messages
+            $ErrorActionPreference = 'SilentlyContinue'
+
+            # Try to connect
+            $Socket.Connect($s, $Port)
+
+            # Make error messages visible again
+            $ErrorActionPreference = 'Continue'
+
+            # Determine if we are connected
+            $obj = New-Object -TypeName PSObject -Property @{
+                Name = $s
+                Connected = $Socket.Connected
+            }    
+            
+            $Results += $obj
+            $Socket.Dispose()
+        } # End foreach $s in $server
+    } # End PROCESS block
+
+    END {
+        Write-Output $Results
+    }
+
+} # End Get-Telnet function
