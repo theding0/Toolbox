b446fd655167e648c4db15b2a7ad18b7fac66897
ahulse <a.hulse90@gmail.com>
2015-06-19 12:49:19 -0500

    Add report
    Created new .ps1 for Mailbox Access reporting

---
+++ Report-MailboxAccess.ps1
@@ -0,0 +1,15 @@
+﻿function Report-MailboxAccess {
+    BEGIN {
+
+        $Mailbox = Get-Mailbox
+
+    } # End BEGIN block
+
+    PROCESS {
+
+    } # End PROCESS block
+
+    END {
+        
+    } # End END block
+}
